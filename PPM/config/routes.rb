a81e300cfa76c50a750a432330c56a021bafa323
marcomorawec <marco.morawec@gmail.com>
2013-06-20 19:48:46 -0400

    route change to home and adding importing bootstrap correctly

--- PPM/config/routes.rb
+++ PPM/config/routes.rb
@@ -1,11 +1,11 @@
 PPM::Application.routes.draw do
-  get "index_controller/ProfileController"
-
-  get "index_controller/CurriculumController"
 
   get "index/Profile"
 
   get "index/Curriculum"
+
+root :to => 'index#home'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -62,5 +62,5 @@ PPM::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 end
