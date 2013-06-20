4314ed0fc881699914ec6b8ffb002f43c7a1915b
Chris VanSchyndel <chris@chris-Aspire-V3-571G.(none)>
2013-06-20 18:47:00 -0400

    Initial Commit

---
+++ PPM/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PPM::Application.config.secret_token = '36c7c3cbad31abc66e7899b986fc42e70d029804e8bf16795a9a8d8facb7f793f0fdc6e65859475266f00da5415a89044d395c2321fef9f9a4dbf1026d336ddb'
